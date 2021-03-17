9cec1a0a097d06532b9fe945d8c898dc581bf36a
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-03-18 00:18:22 +0800

    Unique but non-secret

--- src/auth/FirebaseProvider.tsx
+++ src/auth/FirebaseProvider.tsx
@@ -7,14 +7,16 @@ export interface IFirebaseContext {
 
 export const FirebaseContext = createContext({} as IFirebaseContext);
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyAMD2GwIBu5gW2ZZWm7byXLRT0IP_QIaOA",
+  authDomain: "cheesed-7e555.firebaseapp.com",
+  databaseURL: "https://cheesed-7e555-default-rtdb.firebaseio.com",
+  projectId: "cheesed-7e555",
+  storageBucket: "cheesed-7e555.appspot.com",
+  messagingSenderId: "845021025256",
+  appId: "1:845021025256:web:f6dacad5cd0d1ef74e4a5d",
+  measurementId: "G-FKYKRP5F3L",
 };
 
 const initFirebase = () => {
